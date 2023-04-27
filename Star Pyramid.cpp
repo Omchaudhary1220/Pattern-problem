eab0e3f76bfa9f6b06c17af76ea66fdc7c1e66a8
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-04-27 10:57:45 +0530

    Create Star Pyramid.cpp

---
+++ Star Pyramid.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter the number: ";
+	cin>>n;
+	int i,j,k,p=-1;
+	for(i=1;i<=n;i++){
+	  p++;
+	  for(k=i;k<n;k++){
+	  	cout<<" ";
+	  }
+	  for(j=1;j<=(i+p);j++){
+	  	cout<<"*";
+	  }
+	  cout<<endl;
+	}
+	return 0;
+}
