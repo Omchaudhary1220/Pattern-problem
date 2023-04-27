48ec27830b4b89a4023103e975914a64664a9507
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-04-27 13:01:28 +0530

    Create Binary Number Triangle Pattern.cpp
    1
    0 1
    1 0 1
    0 1 0 1
    1 0 1 0 1

---
+++ Binary Number Triangle Pattern.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+void diamond(int n){
+  int i,j,k;
+  for(i=1;i<=n;i++){
+  	if(i%2==0){
+  		k=0;
+	  }
+	  else{
+	  	k=1;
+	  }
+  	for(j=1;j<=i;j++){
+  		cout<<k<<" ";
+  		if(k==1){
+  			k=0;
+		  }
+		  else{
+		  	k=1;
+		  }
+	  }
+	  cout<<endl;
+  }
+}
+int main(){
+	int n;
+	cout<<"Enter the number: ";
+	cin>>n;
+	diamond(n);
+	return 0;
+}
