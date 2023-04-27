a6e3b6358a4a1c6a2922d778532837a598c39ed1
om kumar chaudhary <117508079+Omchaudhary1220@users.noreply.github.com>
2023-04-27 15:09:25 +0530

    Create Number Crown Pattern.cpp
    1      1
    12    21
    123  321
    12344321

---
+++ Number Crown Pattern.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void diamond(int n){
+  int i,j,k,l;
+  int space=(2*n)-2;
+  for(i=1;i<=n;i++){
+  	for(j=1;j<=i;j++)
+  	cout<<j;
+  	for(l=1;l<=space;l++)
+  	cout<< " ";
+  	for(k=i;k>=1;k--)
+  	cout<<k;
+  	cout<<endl;
+  	space-=2;
+  }
+  
+}
+int main(){
+	int n;
+	cout<<"Enter the number: ";
+	cin>>n;
+	diamond(n);
+	return 0;
+}
